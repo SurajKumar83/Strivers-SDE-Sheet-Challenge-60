5d31b1feb4c1cca1f4c9c95a738aae949f206bde
Suraj Kumar <84279208+SurajKumar83@users.noreply.github.com>
2023-06-15 14:55:50 +0530

    Create Pair_Sum.cpp

---
+++ Pair_Sum.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+vector<vector<int>> pairSum(vector<int> &arr, int s){
+   vector<vector<int>>ans;
+   int n=arr.size();
+   for(int i=0;i<n;i++){
+      int tofind=s-arr[i];
+      for(int j=i+1;j<n;j++){
+          if(tofind==arr[j]){
+             if(arr[i]<=arr[j]){
+               ans.push_back({arr[i],arr[j]});
+             }else{
+                ans.push_back({arr[j],arr[i]});
+             }
+             
+          }
+      }
+   }
+   sort(ans.begin(),ans.end());
+   return ans;
+}
+
