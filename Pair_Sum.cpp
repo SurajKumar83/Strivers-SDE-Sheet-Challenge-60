77fd219594d04e42fe0aa2a19ad7702197e76661
Suraj Kumar <84279208+SurajKumar83@users.noreply.github.com>
2023-06-15 15:05:59 +0530

    Update Pair_Sum.cpp

--- Pair_Sum.cpp
+++ Pair_Sum.cpp
@@ -19,4 +19,33 @@ vector<vector<int>> pairSum(vector<int> &arr, int s){
    sort(ans.begin(),ans.end());
    return ans;
 }
+// 2nd method
+#include <bits/stdc++.h>
+
+vector<vector<int>> pairSum(vector<int> &arr, int s){
+   vector<vector<int>>ans;
+   int n=arr.size();
+   map<int,vector<int>>mp;
+   for(int i=0;i<n;i++){
+      mp[arr[i]].push_back(i);
+   }
+
+   for(int i=0;i<n;i++){
+      int tofind=s-arr[i];
+      if(mp.find(tofind)!=mp.end()){
+         for(auto it:mp[tofind]){
+            if(it>i){
+               if(arr[i]<=tofind){
+                 ans.push_back({arr[i],tofind});
+               }else{
+               ans.push_back({tofind,arr[i]});
+              }
+            }
+         }
+         
+      }
+   }
+    sort(ans.begin(),ans.end());
+   return ans;
+}
 
